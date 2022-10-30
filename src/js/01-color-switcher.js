397ada9e6f54aa574c4dba1f688e992647cf01b3
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2022-10-30 10:24:28 +0100

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+
+
+// const colorСhange = 1000;
+
+// const refs = {
+//     btnStart: document.querySelector(".data-start"),
+//     btnStop: document.querySelector("data-stop"),
+// };
+
+// console.log(btnStart);
+
+// setInterval(() => {
+//    console.log('123');
+// }, colorСhange);
+ 
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// console.log(colorСhange);
